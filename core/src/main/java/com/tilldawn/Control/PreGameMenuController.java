725a9be65a46243a141e67245cf6c77b5e3b76c5
kshyst <kiarash.sh1383@gmail.com>
2025-05-08 00:40:41 +0330

    اینم از گیمش دیگه چی میخوای؟

--- core/src/main/java/com/tilldawn/Control/PreGameMenuController.java
+++ core/src/main/java/com/tilldawn/Control/PreGameMenuController.java
@@ -3,6 +3,7 @@ package com.tilldawn.Control;
 import com.tilldawn.Main;
 import com.tilldawn.Model.GameAssetManager;
 import com.tilldawn.Model.Pregame;
+import com.tilldawn.View.GameView;
 import com.tilldawn.View.PreGameMenuView;
 
 public class PreGameMenuController {
@@ -17,7 +18,8 @@ public class PreGameMenuController {
 
     public void handlePreGameMenuButtons() {
         if (view != null) {
-
+            Main.getMain().getScreen().dispose();
+            Main.getMain().setScreen(new GameView(new GameController(), GameAssetManager.getGameAssetManager().getSkin()));
         }
     }
 
